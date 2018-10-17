5425c1cd29f7b8cd883eaa60627d30cbdac6a5bf
hawks.jamesf <hawks.jamesf@gmail.com>
2018-10-17 10:25:13 +0800

    init project

---
+++ app/src/main/java/com/hawksjamesf/collapsetextview/App.java
@@ -0,0 +1,21 @@
+package com.hawksjamesf.collapsetextview;
+
+import android.app.Application;
+
+import com.vanniktech.emoji.EmojiManager;
+import com.vanniktech.emoji.ios.IosEmojiProvider;
+
+/**
+ * Copyright ® $ 2017
+ * All right reserved.
+ *
+ * @author: hawks.jamesf
+ * @since: Oct/16/2018  Tue
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        EmojiManager.install(new IosEmojiProvider());
+    }
+}
